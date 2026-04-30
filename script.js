7562db23e90a541d7851c114569220a40fabec18
arthur_b_mourao <arthur_b_mourao@estudante.sesisenai.org.br>
2026-04-30 14:28:01 -0300

    Feat:Começando estilziação e configurando biblioteca DaisyUI

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const vento = document.getElementById('vento');
 const precipitacao = document.getElementById('precipitacao');
 const temperatura = document.getElementById('temperatura');
 const tempo = document.getElementById('informacao-tempo');
-
+const body = document.getElementById('body');
 
 
 
@@ -67,7 +67,9 @@ form.addEventListener('submit', async (event) =>{
         definirStatusTempo(rain,cloud_cover);
         
 
-        definirStatusTempo3dias(hoje,latitude,longitude)
+        definirStatusTempo3dias(hoje,latitude,longitude);
+
+
 
     }catch(error){
         console.log(error);
@@ -80,90 +82,82 @@ function definirStatusTempo(rain, cloud_cover){
     if(rain>0){
         if(rain <= 0.3){
             tempo.innerText = 'Chuva fraca, Você não é de açucar';
+        
         }
-        if(rain <=0.6){
+        else if(rain <=0.6){
             tempo.innerText = 'Melhor levar um guarda-chuva';
+            
         }else{
             tempo.innerText = 'Fica em casa que o trem lá fora ta feio';
+            
+        }
+        document.body.classList.add('chuva');
+    }else{
+        document.body.classList.remove('chuva');
+
+        if(cloud_cover >= 0 && cloud_cover <=20){        
+            document.classList.remove('vai-vim-chuva')
+            tempo.innerText = 'Tá bom pra ganhar uma vitamina D';
+        }
+        else if(cloud_cover >= 21 && cloud_cover <=55){
+            document.classList.remove('vai-vim-chuva')
+            tempo.innerText = 'Tempo está nublado';
+        }
+        else{
+            tempo.innerText = 'Tempo fechado';  
+            document.body.classList.add('vai-vim-chuva')
         }
     }
-    if(cloud_cover >= 0 && cloud_cover <=20){
-        tempo.innerText = 'Tá bom pra ganhar uma vitamina D';
-    }
-    else if(cloud_cover >= 21 && cloud_cover <=55){
-        tempo.innerText = 'Tempo está nublado';
-    }
-    else{
-        tempo.innerText = 'Vai vim chuva';
-    }
+    
 }
 
-async function definirStatusTempo3dias(hoje,latitude,longitude){
-
+async function definirStatusTempo3dias(hoje, latitude, longitude) {
     const dias = [
         document.getElementById('dia-1'),
         document.getElementById('dia-2'),
         document.getElementById('dia-3'),
     ];
-    const tempos = [
-        document.getElementById('tempo-1'),
-        document.getElementById('tempo-2'),
-        document.getElementById('tempo-3'),
-    ]
-
     const temperaturas = [
         document.getElementById('temperatura-1'),
         document.getElementById('temperatura-2'),
         document.getElementById('temperatura-3'),
-    ]
-    for(let i = 0; i<3; i++){
+    ];
+    for (let i = 0; i < 3; i++) {
         let data = new Date(hoje);
-        data.setDate(data.getDate()+ i + 1);
-        dias[i].innerText = data.toLocaleDateString('pt-br',{
-            day : 'numeric',
+        data.setDate(data.getDate() + i + 1);
+        dias[i].innerText = data.toLocaleDateString('pt-br', {
+            day: 'numeric',
             month: '2-digit',
         });
 
         let ano = data.getFullYear();
         let mes = String(data.getMonth() + 1).padStart(2, '0');
         let dia = String(data.getDate()).padStart(2, '0');
-
-        let dataUrl = `${ano}-${mes}-${dia}`
+        let dataUrl = `${ano}-${mes}-${dia}`;
 
         try {
             let urlDefinirStatus = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&start_date=${dataUrl}&end_date=${dataUrl}&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto`;
-        
             const tempResStatus = await fetch(urlDefinirStatus);
-
             const tempDataStatus = await tempResStatus.json();
 
-            const{
-                weather_code,
-                temperature_2m_max,
-                temperature_2m_min
-            } = tempDataStatus.daily; 
-
-            let codigoDoTempo = weather_code [i];
-
-            if(codigoDoTempo >=0 && codigoDoTempo <=2){
-                console.log('Ceu Limpo');
-            }else if(codigoDoTempo == 3){
-                console.log('Tempo nublado')
-            }else if(codigoDoTempo >=51 && codigoDoTempo <=57 || codigoDoTempo>=61){
-                console.log('Chuva');
-            }else if(codigoDoTempo == 71 || codigoDoTempo == 73 || codigoDoTempo == 75 || codigoDoTempo == 77){
-                console.log('Nevando')
-            }
-
-            
-            temperaturas[i].innerText = `${temperature_2m_min}/${temperature_2m_max}${[0]}°`;
-            } catch (error) {
-                console.log(error)
+            temperaturas[i].innerText = `${tempDataStatus.daily.temperature_2m_min[0]}°/${tempDataStatus.daily.temperature_2m_max[0]}°`;
+        } catch (error) {
+            console.log(error);
+            temperaturas[i].innerText = 'Erro';
         }
-
-        
     }
+}
 
+function definirBackgorund(weather_code){
+
+    if(weather_code >=0 && weather_code <=2){
+                console.log('Ceu Limpo');
+            }else if(weather_code == 3){
+                console.log('Tempo nublado')
+            }else if(weather_code >=51 && weather_code <=57 || weather_code>=61){
+                console.log('Chuva');
+                
+            }
 }
 
 
